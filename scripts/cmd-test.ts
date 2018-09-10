827d488be9ffc24814f28994fa7cca6763ae0b28
Jeremy Chone <jeremy.chone@gmail.com>
2018-09-09 19:06:03 -0700

    + add npm run test

--- scripts/cmd-test.ts
+++ scripts/cmd-test.ts
@@ -6,8 +6,17 @@ import * as chokidar from 'chokidar';
 
 const servicesDir = 'services';
 
-router({ twatch, dtest }).route();
+router({ test, twatch, dtest }).route();
 
+async function test(serviceName: string, testGrep: string) {
+	if (!serviceName) {
+		console.log("ERROR - test needs to have a service name. Try 'npm run test web-server'");
+		return;
+	}
+	const podName = await getPodName(serviceName);
+
+	await run(false, serviceName, testGrep, podName);
+}
 
 /** run a 'npm run dtest [-g ...]` on a service, which will start a debug session (default host/port) */
 async function dtest(serviceName: string, testGrep: string) {
@@ -17,7 +26,7 @@ async function dtest(serviceName: string, testGrep: string) {
 	}
 
 	// kubectl port-forward $(kubectl get pods -l run=cstar-web-server --no-headers=true -o custom-columns=:metadata.name) 9229
-	const podName = (await spawn('kubectl', ['get', 'pods', '-l', `run=cstar-${serviceName}`, '--no-headers=true', '-o', 'custom-columns=:metadata.name'], { capture: 'stdout' })).stdout.trim();
+	const podName = await getPodName(serviceName);
 	spawn('kubectl', ['port-forward', podName, '9229']);
 
 	return watchAndRun(true, serviceName, testGrep);
@@ -34,7 +43,7 @@ async function twatch(serviceName: string, testGrep: string) {
 }
 
 async function watchAndRun(debug: boolean, serviceName: string, testGrep: string) {
-	const podName = (await spawn('kubectl', ['get', 'pods', '-l', `run=cstar-${serviceName}`, '--no-headers=true', '-o', 'custom-columns=:metadata.name'], { capture: 'stdout' })).stdout.trim();
+	const podName = await getPodName(serviceName);
 
 	const serviceDir = `${servicesDir}/${serviceName}`;
 	const serviceDistDir = `${serviceDir}/dist`;
@@ -49,29 +58,7 @@ async function watchAndRun(debug: boolean, serviceName: string, testGrep: string
 	const watcher = chokidar.watch(`${serviceDistDir}/**/*.js`, { depth: 99, ignoreInitial: true, persistent: true });
 
 	const cr = new CallReducer(async () => {
-		// if debug, we make sure we kill any node process with inspect (to make sure the port is not used)
-		if (debug) {
-			// // TODOto be safe for now, not s
-			// console.log('waiting some before kill the eventual inspec process');
-			// await wait(1000);
-			const args = ['exec', podName];
-			args.push('--', 'pkill', '-f', 'inspec'); // somehow, if we pass "'inpsec'" it fails (ok, because single word)
-			await spawn('kubectl', args, { ignoreFail: true });
-		}
-
-		const args = ['run', 'kexec', serviceName];
-		args.push('--', 'npm', 'run');
-
-		if (debug) {
-			args.push('dtest');
-		} else {
-			args.push('test');
-		}
-
-		if (testGrep) {
-			args.push('--', '-g', testGrep);
-		}
-		spawn('npm', args);
+		return run(debug, serviceName, testGrep, podName);
 	}, 500);
 
 	watcher.on('change', async function (filePath: string) {
@@ -87,4 +74,36 @@ async function watchAndRun(debug: boolean, serviceName: string, testGrep: string
 	// initial call
 	// cr.map();
 
+}
+
+// TODO: need to remove the podName, we probably can call the kexec serviceName -- pkill ...
+async function run(debug = false, serviceName: string, testGrep: string, podName: string) {
+	// if debug, we make sure we kill any node process with inspect (to make sure the port is not used)
+	if (debug) {
+		// // TODOto be safe for now, not s
+		// console.log('waiting some before kill the eventual inspec process');
+		// await wait(1000);
+		const args = ['exec', podName];
+		args.push('--', 'pkill', '-f', 'inspec'); // somehow, if we pass "'inpsec'" it fails (ok, because single word)
+		await spawn('kubectl', args, { ignoreFail: true });
+	}
+
+	const args = ['run', 'kexec', serviceName];
+	args.push('--', 'npm', 'run');
+
+	if (debug) {
+		args.push('dtest');
+	} else {
+		args.push('test');
+	}
+
+	if (testGrep) {
+		args.push('--', '-g', testGrep);
+	}
+	spawn('npm', args);
+}
+
+async function getPodName(serviceName: string) {
+	const podName = (await spawn('kubectl', ['get', 'pods', '-l', `run=cstar-${serviceName}`, '--no-headers=true', '-o', 'custom-columns=:metadata.name'], { capture: 'stdout' })).stdout.trim();
+	return podName;
 }
